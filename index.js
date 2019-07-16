a93d10d02c79155c18620ca8fc60877e4fa38348
Joel-Wheelwright <52959338+Joel-Wheelwright@users.noreply.github.com>
2019-07-16 13:08:54 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+require('dotenv').config({ path: './variables.env'})
+
+mongoose.connect(process.env.DATABASE);
+
+mongoose.connection.on('error', function(error){
+    console.log('Error de mongoose:', error);
+});
+
+require('./Schemas/Persona');
+const modeloPersona = mongoose.model('Persona');
+
+const app = express();
+
+app.use(bodyParser());
+
+app.get('/', function(req, res){
+    res.status(400);
+});
+
+app.get('/hello', function(req, res){
+    res.send('Hello my name is Jeff');
+});
+
+app.get('/nueva-persona', function(req, res){
+
+    const miPersona = new modeloPersona({
+        name: req.query.name,
+        email:req.query.email,
+        username: req.query.username,
+        password: req.query.password,
+        age: req.query.age
+    });
+    miPersona.save().then(function(){
+        res.send('Persona guardada');
+    });
+});
+
+app.get('/personas/joel', function(req, res){
+    console.log(req.query.username);
+    const search = {
+        username: req.query.username
+    };
+    modeloPersona.find(search).then(function(users){
+        res.send(users);
+    });
+});
+
+app.listen(3000, function(){
+    console.log('Aplicacion escuchando en http://localhost:3000');
+});
---
+++ operaciones.js
@@ -0,0 +1,23 @@
+function sumar(a,b){
+    console.log(a+b);
+}
+
+function restar(a,b){
+    console.log(a-b);
+}
+
+function multuplicar(a,b){
+    console.log(a*b);
+}
+
+function dividir(a,b){
+    console.log(a/b);
+}
+
+module.exports = {
+    sumar: sumar,
+    restar: restar,
+    multuplicar: multuplicar,
+    dividir: dividir,
+};
+
